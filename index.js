71efca0562b5e68231b25184f198fffc689d6d16
Eric <56220682+ThEricsson@users.noreply.github.com>
2022-08-10 10:56:52 +0200

    comment added

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-'use strict'
+"use strict";
 
 var mongoose = require("mongoose");
-var url = 'mongodb://localhost:27017/portfolio'
-var app = require('./app');
+var url = "mongodb://localhost:27017/portfolio"; //IP database server
+var app = require("./app");
 var port = 3700;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(url)
-    .then(() => {
-        console.log("Connexio a la BD establerta");
-        app.listen(port, () => {
-            console.log("Servidor funcionant correctament");
-        });
-        
-    })
-    .catch(err => console.log(err));
-    
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connexio a la BD establerta");
+    app.listen(port, () => {
+      console.log("Servidor funcionant correctament");
+    });
+  })
+  .catch((err) => console.log(err));
